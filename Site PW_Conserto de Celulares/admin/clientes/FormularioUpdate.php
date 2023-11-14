67519ce1977766acbabd8475fa659dc3b3f11d2c
Dragonofdark <116024259+Dragonofdark@users.noreply.github.com>
2023-11-14 11:42:01 -0300

    Update FormularioUpdate.php

--- Site PW_Conserto de Celulares/admin/clientes/FormularioUpdate.php
+++ Site PW_Conserto de Celulares/admin/clientes/FormularioUpdate.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="../estilo.css"/>
+    <link rel="stylesheet" href="../../estilo.css"/>
     <title>Clientes - tutorial</title>
 </head>
 <body>
 
     <?php
-        include '../conexao.php';
+        include '../../conexao.php';
         $cli_id = filter_input(INPUT_GET, 'cli_id' , FILTER_DEFAULT);
         $sth = $pdo->prepare("select *from clientes where cli_id = :cli_id");
         $sth->bindValue(':cli_id' , $cli_id);
@@ -35,6 +35,10 @@
                 <div class="load"></div>
                 <input type="hidden" name="cli_id" value="<?= $cli_id ?>"/>
                 <div class="input-field">
+                    <label>Nome do Celular</label>
+                    <input type="text" name="cel_nome" value="<?= $cel_nome ?>">
+                </div>
+                <div class="input-field">
                     <label> Nome </label>
                     <input type="text" name="cli_nome" value="<?= $cli_nome ?>">
                 </div>
@@ -49,8 +53,8 @@
     </div>
 </body>
 
-<script type="text/javascript" src="../jquery-3.7.1.min.js"></script>
+<script type="text/javascript" src="../../jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-<script type="text/javascript" src="../cliente.js"></script>>
+<script type="text/javascript" src="cliente.js"></script>>
 
 </html>

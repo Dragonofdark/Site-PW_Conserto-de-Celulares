9b5a234258e30d8dc519640f73aed7da9fc5ce32
Dragonofdark <116024259+Dragonofdark@users.noreply.github.com>
2023-11-14 11:50:34 -0300

    Update select.php

--- Site PW_Conserto de Celulares/admin/clientes/select.php
+++ Site PW_Conserto de Celulares/admin/clientes/select.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="../estilo.css" />
+    <link rel="stylesheet" href="../../estilo.css" />
     <title>Clientes - tutorial</title>
 </head>
 
@@ -25,12 +25,12 @@
         <div class="load"></div>
         <?php
 
-        include '../conexao.php';
+        include '../../conexao.php';
 
         $sth = $pdo->prepare("select *from clientes order by cli_id DESC");
         $sth->execute();
 
-        echo '<h6>' . $sth->rowCount() . 'Celular(res) Cadastrado(s) </h6>';
+        echo '<h6>' . $sth->rowCount() . 'Cliente(s) Cadastrado(s) </h6>';
         echo '<table class="stripes">';
         echo '<tr>';
         echo '<td> <b> Nome </b> </td>';
@@ -44,9 +44,9 @@
             echo '<td>' . $cli_nome . '</td>';
             echo '<td>' . $cli_email . '</td>';
             echo '<td>';
-            echo '<a href="#" class="deleteClientes" cli_id=' . $cli_id . ' "> Excluir </a>';
+            echo '<a href="#" class="delete" cli_id=' . $cli_id . ' "> Excluir </a>';
             echo ' / ';
-            echo '<a href="formulario_update.php?cli_id=' . $cli_id . ' "> Editar </a>';
+            echo '<a href="formularioUpdate.php?cli_id=' . $cli_id . ' "> Editar </a>';
             echo '<div class="msg" cli_id=' . $cli_id . '></div>';
             echo '</td>';
             echo '</tr>';
@@ -57,7 +57,7 @@
     </div>
 </body>
 
-<script type="text/javascript" src="../jquery-3.7.1.min.js"></script>
+<script type="text/javascript" src="../../jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script type="text/javascript" src="cliente.js"></script>>
 

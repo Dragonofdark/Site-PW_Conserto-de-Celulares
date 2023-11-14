0d66714660aa0d38e5f62bebb238665ad7578d0b
Dragonofdark <116024259+Dragonofdark@users.noreply.github.com>
2023-11-14 11:51:34 -0300

    Update home.php

--- Site PW_Conserto de Celulares/admin/home.php
+++ Site PW_Conserto de Celulares/admin/home.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../estilo.css"/>
+    <link rel="stylesheet" href="../estilo.css"/>
     <title>Painel</title>
 </head>
 
@@ -23,5 +23,5 @@
     </nav>
 </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="../../jquery-3.7.1.main.js"></script>
+    <script type="text/javascript" src="../jquery-3.7.1.main.js"></script>
 </html>
